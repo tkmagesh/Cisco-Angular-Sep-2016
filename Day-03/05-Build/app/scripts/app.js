5b4c9a080d34387f8b18f35ae409fd3f0ac47db5
Magesh <tkmagesh77@gmail.com>
2016-09-09 15:49:12 +0530

    Build

---
+++ Day-03/05-Build/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 13BuildApp
+ * @description
+ * # 13BuildApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('13BuildApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ Day-03/05-Build/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 13BuildApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 13BuildApp
+ */
+angular.module('13BuildApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
