a258c52dfaac1ec68766c47d9b04d0f8b5cfa56e
Magesh <tkmagesh77@gmail.com>
2016-09-09 10:15:26 +0530

    Using moment.js

--- Day-03/01-BugTracker-Storage/utils.js
+++ Day-03/01-BugTracker-Storage/utils.js
@@ -9,3 +9,11 @@ utils.filter("trimText", function(defaultTrimLength){
 		return data.length > trimLength ? data.substr(0,trimLength) + '...' : data;
 	}
 });
+
+utils.value('momentApi', moment);
+
+utils.filter('elapsed', function(momentApi){
+	return function(data){
+		return momentApi(data).fromNow();
+	};
+});
